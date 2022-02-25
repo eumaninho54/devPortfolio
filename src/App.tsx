78ddbd54ae73f3ee1b00672b78e1d99e706d9995
Angelo  Menti <angelo.omarzinho@outlook.com>
2022-02-25 14:06:14 -0300

    transitionInitial finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom'
 
@@ -7,24 +7,35 @@ import Home from './components/home';
 import SplashScreen from './components/splashScreen';
 
 function App() {
+  const [display, setDisplay] = useState("none")
   const aboutMeScroll = useRef(null)
   const skillsScroll = useRef(null)
   const projectsScroll = useRef(null)
+
+
+  setTimeout(() => {
+    setDisplay( () => "initial")
+  }, 3100)
+
   return (
     <BrowserRouter>
-      <SplashScreen/>
+      <SplashScreen />
 
-      <Header 
-        refAboutMe={aboutMeScroll}
-        refSkills={skillsScroll}
-        refProjects={projectsScroll}
-      />
-      
-      <Home 
-        refAboutMe={aboutMeScroll} 
-        refSkills={skillsScroll}
-        refProjects={projectsScroll}
-      />
+      <header style={{transition: "1s", display: display}}>
+        <Header
+          refAboutMe={aboutMeScroll}
+          refSkills={skillsScroll}
+          refProjects={projectsScroll}
+        />
+      </header>
+
+      <main style={{transition: "1s", display: display}}>
+        <Home
+          refAboutMe={aboutMeScroll}
+          refSkills={skillsScroll}
+          refProjects={projectsScroll}
+        />
+      </main>
 
     </BrowserRouter>
   );
